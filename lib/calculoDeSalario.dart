c621fd2ed0795b3210ea02b39a8bc34b88eff530
souza99 <jpfsouza99@gmail.com>
2022-06-13 16:39:07 -0300

    inteface inplementada

--- lib/calculoDeSalario.dart
+++ lib/calculoDeSalario.dart
@@ -2,15 +2,52 @@
 
 import 'dart:ffi';
 import 'dart:html';
+import 'dart:io';
 
 void main(List<String> aguments){
+  int? opcao;
 
   print('''Escolha uma opção
 1 - calculo de valor de salario bruto
-2 - calculo de desconto INSS
-3 - calculo de desconto ticket alimentação
+2 - calculo de salario com desconto INSS
+3 - calculo de desconto de vales
 4 - calculo de salario liquido
 ''');
+String? resposta;
+
+do{
+  print('Aresposta não pode ser nula');
+  resposta = stdin.readLineSync();
+}while(resposta == null);
+  opcao = int.parse(resposta);
+
+  if(opcao <1 || opcao > 4){
+    print('Essa opção não está disponivel');
+  }
+  else if(opcao == 1){
+    double? horas;
+    double? valorHora;
+    calculaValorSalario(horas!, valorHora!);
+  }else if(opcao == 2){
+    double? valorSalarioBruto;
+    descontoSalarioInss(valorSalarioBruto!);
+  }else if(opcao == 3){
+
+    double? valeAlimentacao;
+    double? valerRefeicao;
+    double? valeCultura;
+    double? valeAcademia;
+    double? valeCombustivel;
+    double? valePresente;
+
+    descontoVales(valeAlimentacao!, valerRefeicao!, valeCultura!, valeAcademia!, valeCombustivel!, valePresente!);
+  }else if(opcao == 4){
+    double? valorSalarioBruto;
+    double? descontoInss;
+    double? descontoVale;
+    calculoSalarioLiquido(valorSalarioBruto!, descontoInss!, descontoVale!);
+  }
+
 }
 
 
